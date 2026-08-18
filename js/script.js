38cd995e365f15ea5e833b4c512a31f2fd931c22
GoldyRoger07 <latouche822@gmail.com>
2026-08-18 15:27:58 -0400

    787

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const logo = document.querySelector("#logo"),
+      title = document.querySelector("#title"),
+      btn = document.querySelector("#btn")
+
+      document.addEventListener("DOMContentLoaded",()=>{
+        title.classList.add("active")
+        btn.classList.add("active")
+        logo.classList.add("active")
+      })
